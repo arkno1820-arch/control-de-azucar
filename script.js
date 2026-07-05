4ed75ad61a803f82e732c988b0d16490eefeee4c
arkno1820 <arkno1820@gmail.com>
2026-07-05 17:34:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONFIGURACIÓN - CAMBIA ESTO SEGÚN TUS DATOS
 // ============================================================
 const PIN_CORRECTO = '1234';
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby1cKQ4gZ_mR-COC-WoyFAL7GEjHWqwjlKkfCH9NhosnU7mHl0S_zSUJ-pj1ILccCoM/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx89YxcgDPW1NRjjYLLf_XwyDLzDB-K_GpM-IMpWOhOaDwZUVCqOqRzKd_90u2orTpm/exec';
 const EMAIL_DESARROLLADOR = 'cesarandresmanriquezfigueroa@gmail.com';
 
 // ============================================================
