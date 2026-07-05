84c678e08ff48c92fe275bdd2476a5c651d7e3d5
arkno1820 <arkno1820@gmail.com>
2026-07-05 20:16:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // CONFIGURACIÓN - ACTUALIZADO CON URL FUNCIONAL
 // ============================================================
 const PIN_HASH = 'fce1eda2d2a507fea1c09ef0bb92500280534c3d9c35418b87cd41fb4239de93'; // Hash de "1234"
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxtrMWunujQBg9Vs7z2scO-vPqeZnaHSBBqOdgzwQ8LUcfvKeE_uHbAz4YWo8qEo7ykug/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyBWKi3PXekZgNxhhoqSYz1en7CGw4CD-6iVDT2kIqsSqPUIYwNDybwzUQnwi9UybFs/exec';
 const EMAIL_DESARROLLADOR = 'cesarandresmanriquezfigueroa@gmail.com';
 
 // ============================================================
